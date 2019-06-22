e005ef7fa2dd6e73a32a823d1c5d8b0f622feb78
Goran Gajic <fero.null@gmail.com>
2019-06-22 11:48:35 +0200

    change Constants import

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, ScrollView, Text, View, Image } from 'react-native';
-import { Constants } from 'expo';
+import Constants from 'expo-constants'
 
 export default function App() {
   return (
